4b6f20fcba48ec38d281a30c5a341b52c750da55
Nivek <vallhalla@qq.com>
2023-11-11 15:49:53 +0800

    decomposed packages and use logger instead of print

---
+++ routers/useCase.py
@@ -0,0 +1,72 @@
+# 网络请求
+from fastapi import APIRouter, File, UploadFile
+
+# 数据结构
+from utils.data import ChatRequest
+
+# 工具库
+from datetime import datetime
+import os
+from uuid import uuid4
+
+# 日志
+import logging
+logger = logging.getLogger(__name__)
+
+
+use_case_router = APIRouter(prefix='/useCase')
+
+@use_case_router.post("/")
+async def processText(long_text: ChatRequest):
+    
+    # 文本模版
+    from internal.template import makeChain
+    
+    # 解析器
+    from utils.parser import parseJson
+    
+    chain = makeChain(None)
+    
+    # 处理传递的长文本数据
+    message = long_text.message
+    logger.info(f"对话：{message}")
+    # 进行文本处理逻辑
+    ans = chain.invoke(message)  # 这里只是一个示例
+    logger.info(f"回答：{ans}")
+    
+    response = parseJson(ans)
+    return response
+
+@use_case_router.post("/retriever")
+async def uploadRetriver(file: UploadFile = File(...)):
+    
+    # 加载器
+    from utils.loader import loadMD
+    
+    # 解析器
+    from utils.parser import parseDocument
+    
+    # 文本模版
+    from internal.template import makeChain    
+    # 寻回模型加载
+    from internal.retriever import makeRetriever
+    
+    
+    timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+    unique_id = str(uuid4())
+    
+    if file.filename:
+        file_extension = file.filename.split(".")[-1]
+        new_filename = f"{timestamp}_{unique_id}.{file_extension}"
+        file_path = os.path.join("upload_folder", new_filename)
+        
+        with open(file_path, "wb") as file_object:
+            file_object.write(file.file.read())
+            
+        requirements_json = parseDocument(loadMD(file_path).split('\n'))
+        logger.info(requirements_json)
+        
+        retriever = makeRetriever(requirements_json)
+        chain = makeChain(retriever)
+        
+        return {"message":"Success"}
---
+++ server.py
@@ -0,0 +1,44 @@
+# api前缀与端口号
+API_PREFIX = '/v1'
+PORT = 11451
+
+# 日志
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="\n[%(asctime)s - {%(module)s.py (%(lineno)d)} - %(funcName)s()]\n %(message)s\n",
+    datefmt="%Y-%m-%d,%H:%M:%S",
+)
+
+# FastAPI
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+# 处理跨域请求
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# 注册路由
+from routers import use_case_router
+app.include_router(use_case_router, prefix=API_PREFIX)
+
+from routers import chat_router
+app.include_router(chat_router, prefix=API_PREFIX)
+
+
+# 服务器根应答
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World"}
+
+
+# 程序入口
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=PORT, workers=1)
